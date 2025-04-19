9ec0ff39f19beab359d11877eed444cc4c7a8572
roozbeh04 <sif3940@gmail.com>
2025-04-20 00:31:01 +0330

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ WC_API_URL = "https://www.jamakshop.ir/wp-json/wc/v3"
 CONSUMER_KEY = "ck_48630eb2196cb3a83f5ad5e3dc82c1c15d911068"
 CONSUMER_SECRET = "cs_dc09790cafab09f23ad3a2ab79032b7c87abd3b1"
 BOT_TOKEN = "7922260637:AAEtLE-bBM4mJSkYWkG89ggxBBR2dxYQRqA"
-SUPPORT_LINK = "https://t.me/sif39"
+SUPPORT_LINK = "https://t.me/sif3940"
 ADMIN_EMAIL = 7922260637  # شناسه عددی ادمین یا همان ربات برای ارسال سفارشات
