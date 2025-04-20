c4c8cd21207a7dfb7b95661b7499da87100c2a89
roozbeh04 <sif3940@gmail.com>
2025-04-20 21:00:44 +0330

    Update config.py
    Update file

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ WC_URL = "https://www.jamakshop.ir/wp-json/wc/v3"
 WC_KEY = "ck_48630eb2196cb3a83f5ad5e3dc82c1c15d911068"
 WC_SECRET = "cs_dc09790cafab09f23ad3a2ab79032b7c87abd3b1"
 
-SUPPORT_TELEGRAM = "https://t.me/sif39"
-SUPPORT_TELEGRAM_USERNAME = "sif39"
+SUPPORT_TELEGRAM = "https://t.me/sif3940"
+SUPPORT_TELEGRAM_USERNAME = "sif3940"
 WHATSAPP_LINK = "https://wa.me/989120000000"
