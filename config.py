99965a5285d75754382113b5281a5ed53c6faf81
roozbeh04 <sif3940@gmail.com>
2025-04-21 20:59:41 +0330

    Update config.py
    update

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+# ------------------ پیکربندی ------------------
+
 BOT_TOKEN = "7922260637:AAEtLE-bBM4mJSkYWkG89ggxBBR2dxYQRqA"
+
 WC_URL = "https://www.jamakshop.ir/wp-json/wc/v3"
 WC_KEY = "ck_48630eb2196cb3a83f5ad5e3dc82c1c15d911068"
 WC_SECRET = "cs_dc09790cafab09f23ad3a2ab79032b7c87abd3b1"
-SUPPORT_USERNAME = "sif3940"
-NEWSLETTER_EMAIL = "sif3940@gmail.com"
-SITE_URL = "https://www.jamakshop.ir"
+
+SUPPORT_TELEGRAM = "https://t.me/sif3940"
+SUPPORT_TELEGRAM_USERNAME = "sif3940"
+WHATSAPP_LINK = "https://wa.me/989171282075"
 INSTAGRAM_URL = "https://instagram.com/jamakshop1"
